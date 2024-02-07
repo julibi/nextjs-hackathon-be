99afa90a5b6c516ccaa4b9234c9ad4183de63842
Joost Duecker <joost.duecker@uxma.com>
2024-02-07 16:35:28 +0100

    Add openapi

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Next.js Hackathon Backend - Todo API')
+    .setDescription('Happy coding')
+    .setVersion('1.0')
+    .addTag('todo')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
